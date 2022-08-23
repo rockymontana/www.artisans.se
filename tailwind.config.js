50c7b455443abd8dfea976d9d9309b8f4371bff1
thinkverse <hallberg.kim@gmail.com>
2022-08-24 00:32:12 +0200

    Update `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.{blade.php,js}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['ONE DAY', 'Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    }
 }
